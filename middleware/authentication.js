e5b705eb9952a10ed7a6e42e59d97be8eb1c65ab
TuanTai169 <nguyentuantai412000@gmail.com>
2021-11-02 14:35:59 +0700

    update auth

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -5,7 +5,7 @@ const checkAdmin = async (req, res, next) => {
   try {
     const user = await User.findById(req.userId)
     if (user.roles === "EMPLOYEE" || user.roles === "MANAGER")
-      return res.status(200).json({
+      return res.status(403).json({
         success: false,
         message: "You are not allowed to make this request",
       })
@@ -24,7 +24,7 @@ const checkManager = async (req, res, next) => {
   try {
     const user = await User.findById(req.userId)
     if (user.roles === "EMPLOYEE")
-      return res.status(200).json({
+      return res.status(403).json({
         success: false,
         message: "User permission",
       })
