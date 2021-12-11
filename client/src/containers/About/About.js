f37b9716dcb3626e51f55179daf29b4cf1fdb8c4
Tai Nguyen Tuan <nguyentuantai412000@gmail.com>
2021-12-11 17:33:25 +0700

    update about

--- client/src/containers/About/About.js
+++ client/src/containers/About/About.js
@@ -3,9 +3,9 @@ import React from "react"
 const About = () => {
   return (
     <div>
-      <h2>DTHSOFT - GROUP 08</h2>
-      <h6>Nguyen Tuan Tai - 18110193</h6>
-      <h6>Vo Anh Huy - 18110127</h6>
+      <h2>DTHSOFT - GROUP 31</h2>
+      <h6>Tran Vo Buu Dien - 18110096</h6>
+      <h6>Nguyen Tuan Tai - 181100193</h6>
     </div>
   )
 }
